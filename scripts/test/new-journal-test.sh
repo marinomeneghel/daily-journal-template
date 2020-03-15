c0e4f32c8771704f752b6f1e47d87b0497ced2ad
Marino Meneghel <marino.meneghel@protonmail.com>
2020-03-15 17:44:34 +0100

    Add (disabled) test to check script executes when all ok

--- scripts/test/new-journal-test.sh
+++ scripts/test/new-journal-test.sh
@@ -3,3 +3,10 @@
   [ "$status" -eq 1 ]
   [ "${lines[0]}" = "No title argument supplied" ]
 }
+
+@test "checks completes correctly when title is passed" {
+  skip "need to figure out how to handle the bit that opens the file for editing"
+  run ~/Documents/daily-journal-template/scripts/new-journal.sh 'A nice title'
+  [ "$status" -eq 0 ]
+  echo $output >&3
+}
