c4f6300f12908571c46f25fa71fad5108b931446
Marino Meneghel <marino.meneghel@protonmail.com>
2020-03-16 23:59:42 +0100

    Test filename gets created from title
    Extract multiple functions to ease testing

--- bin/new-journal.sh
+++ bin/new-journal.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
+check_script_arguments() {
+  if [ -z "$1" ]
+  then
+    echo "No title argument supplied"
+    exit 1
+  fi
+}
+
+build_filename_from_title() {
+  local title=$1
+  local currentdate=`date +"%d-%b"`
+  local formattedtitle=${title// /-}
+  echo [$currentdate]-$formattedtitle.md
+}
+
+run_main() {
+  local title=$1
+  check_script_arguments $title
+  filename=build_filename_from_title $title
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
 then
-  echo "No title argument supplied"
-  exit 1
+    run_main
 fi
-
-title=$1
-currentdate=`date +"%d-%b"`
-formattedtitle=${title// /-}
-
--- test/new-journal-test.sh
+++ test/new-journal-test.sh
@@ -1,10 +1,23 @@
+journal_script='./bin/new-journal.sh'
+
+setup() {
+  source ${journal_script}
+}
+
 @test "fails with error when title arg is not passed" {
-  run ~/Documents/daily-journal-template/scripts/new-journal.sh
+  run check_script_arguments
   [ "$status" -eq 1 ]
   [ "${lines[0]}" = "No title argument supplied" ]
 }
 
 @test "checks completes correctly when title is passed" {
-  run ~/Documents/daily-journal-template/scripts/new-journal.sh 'A nice title'
+  run check_script_arguments 'A nice title'
+  [ "$status" -eq 0 ]
+}
+
+@test "formats given title adding day and month" {
+  run build_filename_from_title 'A nice title'
+  echo $output
+  [ "$output" = "[16-Mar]-A-nice-title.md" ]
   [ "$status" -eq 0 ]
 }
